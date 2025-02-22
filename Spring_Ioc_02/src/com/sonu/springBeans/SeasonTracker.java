f9d745acc28d962204e68b0afecb304395901376
Sambit Kumar Swain <141361224+9861164945@users.noreply.github.com>
2025-02-22 20:49:34 +0530

    Using @Annotaionand Xml Based COndiguration

--- Spring_Ioc_02/src/com/sonu/springBeans/SeasonTracker.java
+++ Spring_Ioc_02/src/com/sonu/springBeans/SeasonTracker.java
@@ -1,4 +1,6 @@
-package com.sonu.springBeans;
+
+//Xml Driven
+/*package com.sonu.springBeans;
 
 import java.time.LocalDate;
 
@@ -28,6 +30,51 @@ public class SeasonTracker
 	        return name+" Now Winter Season " ;
 	    }
 	}
+
+	}
+
+
+
+*/
+//@ anonation and xml driven Configuration
+
+package com.sonu.springBeans;
+
+import java.time.LocalDate;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component("ses")
+public class SeasonTracker 
+{
+	 
+	private LocalDate date;
+	//setter method for Setter Injection(alt+shift+s+R);
+	@Autowired
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+	//Business method
+	
+	public String showSeason(String name) 
+	{
+	    int month = date.getMonthValue();
+	    if (month >= 3 && month <= 5) 
+	    {
+	        return  name +" Now Summer Season " ;
+	    }
+	    else if (month >= 6 && month <= 8) 
+	    {
+	        return  name+" Now Rainy Season " ;
+	    }
+	    else 
+	    {
+	        return name+" Now Winter Season " ;
+	    }
+	}
+
+	
 
 	}
 
